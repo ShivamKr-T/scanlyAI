e4e1e93112a42543fef7ab354a3aa5a091375185
ShivamKr-T <kumarshivam63834@gmail.com>
2025-07-19 19:48:24 +0530

    Implemented File uploader and handling uploads

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -26,7 +26,7 @@ export default function Home() {
 
     <section className="main-section">
       <div className="page-heading py-16">
-        <h1>Track Yout Applications & Resume Ratings</h1>
+        <h1>Track Your Applications & Resume Ratings</h1>
         <h2>Review your submissions and check AI-powered feedback</h2>
       </div>
 
